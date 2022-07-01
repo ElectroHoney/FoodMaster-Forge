7eac11304db4b9da80b3f1adf6fadfb194e252df
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-01 17:18:08 +0300

    feat: add main mod class

--- src/main/java/net/electrohoney/foodmastermod/FoodMaster.java
+++ src/main/java/net/electrohoney/foodmastermod/FoodMaster.java
@@ -1,21 +1,23 @@
 package net.electrohoney.foodmastermod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
+import net.electrohoney.foodmastermod.block.ModBlocks;
+import net.electrohoney.foodmastermod.block.entity.ModBlockEntities;
+import net.electrohoney.foodmastermod.item.ModItems;
+import net.electrohoney.foodmastermod.recipe.ModRecipes;
+import net.electrohoney.foodmastermod.screen.ModMenuTypes;
+import net.electrohoney.foodmastermod.screen.PotBlockScreen;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(FoodMaster.MOD_ID)
@@ -27,12 +29,24 @@ public class FoodMaster
 
     public FoodMaster()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+        ModBlockEntities.register(eventBus);
+        ModMenuTypes.register(eventBus);
+        ModRecipes.register(eventBus);
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event){
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.POT_BLOCK.get(), RenderType.translucent());
+
+        MenuScreens.register(ModMenuTypes.POT_BLOCK_MENU.get(), PotBlockScreen::new);
     }
 
     private void setup(final FMLCommonSetupEvent event)
