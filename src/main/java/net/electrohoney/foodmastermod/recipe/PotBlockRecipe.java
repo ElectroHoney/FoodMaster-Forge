1a6b108cfcd4891cc528062aef7a835433cda40b
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-01 17:21:32 +0300

    feat: add pot shapeless block recipe class

---
+++ src/main/java/net/electrohoney/foodmastermod/recipe/PotBlockRecipe.java
@@ -0,0 +1,133 @@
+package net.electrohoney.foodmastermod.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.electrohoney.foodmastermod.FoodMaster;
+import net.electrohoney.foodmastermod.block.entity.custom.PotBlockEntity;
+import net.minecraft.core.NonNullList;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+
+public class PotBlockRecipe implements Recipe<SimpleContainer> {
+//    @todo I have to modify the recipe so its not shapeless
+    private final ResourceLocation id;
+    private final ItemStack output;
+    private final NonNullList<Ingredient> recipeItems;
+
+    public PotBlockRecipe(ResourceLocation id, ItemStack output, NonNullList<Ingredient> recipeItems) {
+        this.id = id;
+        this.output = output;
+        this.recipeItems = recipeItems;
+    }
+
+    @Override
+    public boolean matches(SimpleContainer pContainer, Level pLevel) {
+        return recipeItems.get(0).test(pContainer.getItem(PotBlockEntity.RECIPE_SLOT_ID));
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer pContainer) {
+        return output;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int pWidth, int pHeight) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem() {
+        return output.copy();
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+    public static class Type implements RecipeType<PotBlockRecipe>{
+        private Type(){}
+            public static final Type INSTANCE = new Type();
+            public static final String ID = "boiling";
+    }
+
+    //thanks a lot Kaupenjoe!! :)
+    //a modified version of this from https://github.com/Tutorials-By-Kaupenjoe/Forge-Tutorial-1.18.1/blob/45-recipeTypes/src/main/java/net/kaupenjoe/tutorialmod/recipe/GemCuttingStationRecipe.java
+    public static class Serializer implements RecipeSerializer<PotBlockRecipe> {
+        public static final Serializer INSTANCE = new Serializer();
+        public static final ResourceLocation ID =
+                new ResourceLocation(FoodMaster.MOD_ID,"boiling");
+
+        @Override
+        public PotBlockRecipe fromJson(ResourceLocation id, JsonObject json) {
+            ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "output"));
+
+            JsonArray ingredients = GsonHelper.getAsJsonArray(json, "ingredients");
+            NonNullList<Ingredient> inputs = NonNullList.withSize(1, Ingredient.EMPTY);
+
+            for (int i = 0; i < inputs.size(); i++) {
+                inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
+            }
+
+            return new PotBlockRecipe(id, output, inputs);
+        }
+
+        @Override
+        public PotBlockRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
+            NonNullList<Ingredient> inputs = NonNullList.withSize(buf.readInt(), Ingredient.EMPTY);
+
+            for (int i = 0; i < inputs.size(); i++) {
+                inputs.set(i, Ingredient.fromNetwork(buf));
+            }
+
+            ItemStack output = buf.readItem();
+            return new PotBlockRecipe(id, output, inputs);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf buf, PotBlockRecipe recipe) {
+            buf.writeInt(recipe.getIngredients().size());
+            for (Ingredient ing : recipe.getIngredients()) {
+                ing.toNetwork(buf);
+            }
+            buf.writeItemStack(recipe.getResultItem(), false);
+        }
+
+        @Override
+        public RecipeSerializer<?> setRegistryName(ResourceLocation name) {
+            return INSTANCE;
+        }
+
+        @Nullable
+        @Override
+        public ResourceLocation getRegistryName() {
+            return ID;
+        }
+
+        @Override
+        public Class<RecipeSerializer<?>> getRegistryType() {
+            return Serializer.castClass(RecipeSerializer.class);
+        }
+
+        @SuppressWarnings("unchecked") // Need this wrapper, because generics
+        private static <G> Class<G> castClass(Class<?> cls) {
+            return (Class<G>)cls;
+        }
+    }
+}
