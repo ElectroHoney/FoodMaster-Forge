54390bf1506d3715b13b66221e8f1df9c8bbc739
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-01 17:19:15 +0300

    feat: add mod event register class

---
+++ src/main/java/net/electrohoney/foodmastermod/event/ModEventBusEvents.java
@@ -0,0 +1,26 @@
+package net.electrohoney.foodmastermod.event;
+
+import net.electrohoney.foodmastermod.FoodMaster;
+import net.electrohoney.foodmastermod.block.custom.PotBlock;
+import net.electrohoney.foodmastermod.recipe.PotBlockRecipe;
+import net.minecraft.core.Registry;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nonnull;
+
+@Mod.EventBusSubscriber(modid = FoodMaster.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvents {
+//    @SubscribeEvent
+//    public static void registerModifierSerializers(@Nonnull final RegistryEvent.Register<GlobalLootModifierSerializer<?>> event){
+//
+//    }
+
+    @SubscribeEvent
+    public static void registerRecipeTypes(@Nonnull final RegistryEvent.Register<RecipeSerializer<?>> event){
+        Registry.register(Registry.RECIPE_TYPE, PotBlockRecipe.Type.ID, PotBlockRecipe.Type.INSTANCE);
+    }
+}
