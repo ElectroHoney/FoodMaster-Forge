f19a20b84e0e21bd1e4499d1185853d4502257d3
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-03 22:07:27 +0300

    feat: add fluid rendering using function from JEI

--- src/main/java/net/electrohoney/foodmastermod/screen/PotBlockScreen.java
+++ src/main/java/net/electrohoney/foodmastermod/screen/PotBlockScreen.java
@@ -1,22 +1,44 @@
 package net.electrohoney.foodmastermod.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Matrix4f;
+import jdk.jfr.FlightRecorder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.ingredients.IIngredientRenderer;
 import net.electrohoney.foodmastermod.FoodMaster;
+import net.electrohoney.foodmastermod.block.entity.custom.PotBlockEntity;
+import net.electrohoney.foodmastermod.screen.renderer.FluidStackRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Widget;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.fluids.FluidAttributes;
+import net.minecraftforge.fluids.FluidStack;
 
 public class PotBlockScreen extends AbstractContainerScreen<PotBlockMenu> {
     private static final ResourceLocation TEXTURE =
             new ResourceLocation(FoodMaster.MOD_ID, "textures/gui/pot_block_gui.png");
     //@todo overwrite image width to correct menu(did that, I should delete this todo)
     private static final int imageWidth = 194;
+    private static final int imageHeight = 166;
 
+    private final IDrawable overlay = null;
     public PotBlockScreen(PotBlockMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
+    }
+
+    @Override
+    protected void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
+        super.renderLabels(pPoseStack, pMouseX, pMouseY);
+        drawString(pPoseStack, this.font, "Fluid"+ this.menu.getFluidAmount(),28, 28, 0xffffff);
     }
 
     @Override
@@ -30,7 +52,7 @@ public class PotBlockScreen extends AbstractContainerScreen<PotBlockMenu> {
         this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight);
         int t = this.menu.getScaledTemperature();
         //System.out.print("Temp:" + t + "\n");
-        //todo this is good but for some reason the bar is too low(nope its perfect)
+        //todo this is good but for some reason the bar is too low(nope its perfect)(I looked at how the furnace was drawing the flame)
         blit(pPoseStack, x + 35, y + 16 + 53 - t, 195,  70-t,  7, t);
 
 
@@ -39,6 +61,122 @@ public class PotBlockScreen extends AbstractContainerScreen<PotBlockMenu> {
 //          //x and y of arrow, offset of arrow progress, vertical offset of progress, width of image, height of image
             blit(pPoseStack, x + 110, y + 35, 195, 0,  menu.getScaledProgress(), 16);
         }
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0,
+                getStillFluidSprite(this.menu.getFluidStack()).getName());
+
+        //blit(pPoseStack, x+102, y+41, 0, 0, 16, 16);
+        FluidStackRenderer renderer = new FluidStackRenderer(PotBlockEntity.POT_MAX_FLUID_CAPACITY, true, 12, 35);
+        renderer.render(pPoseStack, x+10, y+(35/4*PotBlockEntity.POT_MAX_FLUID_CAPACITY)/2000, new FluidStack(Fluids.WATER, menu.getFluidAmount()));
+//        renderer.render(pPoseStack, x+20, y+(35/4*PotBlockEntity.POT_MAX_FLUID_CAPACITY)/2000, new FluidStack(Fluids.WATER, 3000));
+//        renderer.render(pPoseStack, x+30, y+(35/4*PotBlockEntity.POT_MAX_FLUID_CAPACITY)/2000, new FluidStack(Fluids.WATER, 4000));
+    }
+
+    private void renderFluid(PoseStack pPoseStack) {
+        RenderSystem.enableBlend();
+        drawFluid(pPoseStack, 16, 16, new FluidStack(Fluids.WATER, 2000));
+        RenderSystem.setShaderColor(1, 1, 1, 1);
+//        if(overlay!=null){
+//            pPoseStack.pushPose();{
+//                pPoseStack.translate(0,0,200);
+//                overlay.draw(pPoseStack);
+//            }
+//            pPoseStack.popPose();
+//        }
+        RenderSystem.disableBlend();
+    }
+
+    public static final int TEXTURE_SIZE = 16;
+    private static final int MIN_FLUID_HEIGHT = 1; // ensure tiny amounts of fluid are still visible
+
+    private static void setGLColorFromInt(int color) {
+        float red = (color >> 16 & 0xFF) / 255.0F;
+        float green = (color >> 8 & 0xFF) / 255.0F;
+        float blue = (color & 0xFF) / 255.0F;
+        float alpha = ((color >> 24) & 0xFF) / 255F;
+
+        RenderSystem.setShaderColor(red, green, blue, alpha);
+    }
+    private void drawFluid(PoseStack poseStack, final int width, final int height, FluidStack fluidStack) {
+        Fluid fluid = fluidStack.getFluid();
+        if (fluid.isSame(Fluids.EMPTY)) {
+            return;
+        }
+
+        TextureAtlasSprite fluidStillSprite = getStillFluidSprite(fluidStack);
+
+        FluidAttributes attributes = fluid.getAttributes();
+        int fluidColor = attributes.getColor(fluidStack);
+
+        long amount = fluidStack.getAmount();
+        long scaledAmount = (amount * height) / PotBlockEntity.POT_MAX_FLUID_CAPACITY;
+        if (amount > 0 && scaledAmount < MIN_FLUID_HEIGHT) {
+            scaledAmount = MIN_FLUID_HEIGHT;
+        }
+        if (scaledAmount > height) {
+            scaledAmount = height;
+        }
+
+        drawTiledSprite(poseStack, width, height, fluidColor, scaledAmount, fluidStillSprite);
+    }
+    //taken from JEI. thanks a lot mezz!
+    private void  drawTiledSprite(PoseStack poseStack, final int tiledWidth, final int tiledHeight, int color, long scaledAmount, TextureAtlasSprite sprite) {
+        RenderSystem.setShaderTexture(0, InventoryMenu.BLOCK_ATLAS);
+        Matrix4f matrix = poseStack.last().pose();
+        setGLColorFromInt(color);
+
+        final int xTileCount = tiledWidth / TEXTURE_SIZE;
+        final int xRemainder = tiledWidth - (xTileCount * TEXTURE_SIZE);
+        final long yTileCount = scaledAmount / TEXTURE_SIZE;
+        final long yRemainder = scaledAmount - (yTileCount * TEXTURE_SIZE);
+
+        final int yStart = tiledHeight;
+
+        for (int xTile = 0; xTile <= xTileCount; xTile++) {
+            for (int yTile = 0; yTile <= yTileCount; yTile++) {
+                int width = (xTile == xTileCount) ? xRemainder : TEXTURE_SIZE;
+                long height = (yTile == yTileCount) ? yRemainder : TEXTURE_SIZE;
+                int x = (xTile * TEXTURE_SIZE);
+                int y = yStart - ((yTile + 1) * TEXTURE_SIZE);
+                if (width > 0 && height > 0) {
+                    long maskTop = TEXTURE_SIZE - height;
+                    int maskRight = TEXTURE_SIZE - width;
+
+                    drawTextureWithMasking(matrix,  155, 97, sprite, maskTop, maskRight, 100);
+                }
+            }
+        }
+    }
+
+    private static void drawTextureWithMasking(Matrix4f matrix,
+                                               float xCoord, float yCoord, TextureAtlasSprite textureSprite, long maskTop, long maskRight, float zLevel) {
+        float uMin = textureSprite.getU0();
+        float uMax = textureSprite.getU1();
+        float vMin = textureSprite.getV0();
+        float vMax = textureSprite.getV1();
+        uMax = uMax - (maskRight / 16F * (uMax - uMin));
+        vMax = vMax - (maskTop / 16F * (vMax - vMin));
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+
+        Tesselator tessellator = Tesselator.getInstance();
+        BufferBuilder bufferBuilder = tessellator.getBuilder();
+        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        bufferBuilder.vertex(matrix, xCoord, yCoord + 16, zLevel).uv(uMin, vMax).endVertex();
+        bufferBuilder.vertex(matrix, xCoord + 16 - maskRight, yCoord + 16, zLevel).uv(uMax, vMax).endVertex();
+        bufferBuilder.vertex(matrix, xCoord + 16 - maskRight, yCoord + maskTop, zLevel).uv(uMax, vMin).endVertex();
+        bufferBuilder.vertex(matrix, xCoord, yCoord + maskTop, zLevel).uv(uMin, vMin).endVertex();
+        tessellator.end();
+    }
+
+    private static TextureAtlasSprite getStillFluidSprite(FluidStack fluidStack){
+        Minecraft minecraft1 = Minecraft.getInstance();
+        Fluid fluid = fluidStack.getFluid();
+        FluidAttributes attributes = fluid.getAttributes();
+        ResourceLocation fluidStill = attributes.getStillTexture(fluidStack);
+        return minecraft1.getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(fluidStill);
     }
 
     @Override
