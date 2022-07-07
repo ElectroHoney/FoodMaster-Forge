fcf22160be1ce65f4c96a0d1345e771448ce5e8e
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-07 18:34:15 +0300

    test: empty test for pot block

---
+++ src/test/java/net/electrohoney/foodmastermod/block/custom/PotBlockTest.java
@@ -0,0 +1,10 @@
+package net.electrohoney.foodmastermod.block.custom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PotBlockTest {
+
+    @org.junit.jupiter.api.Test
+    void getShape() {
+    }
+}
