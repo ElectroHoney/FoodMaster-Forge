c59dc3e44bc7ee13d99312bff69e4109afad6e55
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-07 18:41:28 +0300

    feat: jei registered catalyst and clickable area around arrow for ease of access of JEI recipes

--- src/main/java/net/electrohoney/foodmastermod/integration/JEIFoodMasterPlugin.java
+++ src/main/java/net/electrohoney/foodmastermod/integration/JEIFoodMasterPlugin.java
@@ -3,12 +3,14 @@ package net.electrohoney.foodmastermod.integration;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.recipe.RecipeType;
-import mezz.jei.api.registration.IRecipeCategoryRegistration;
-import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.*;
 import net.electrohoney.foodmastermod.FoodMaster;
+import net.electrohoney.foodmastermod.block.ModBlocks;
 import net.electrohoney.foodmastermod.recipe.PotBlockRecipe;
+import net.electrohoney.foodmastermod.screen.screens.PotBlockScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
 
 import java.util.List;
@@ -27,6 +29,22 @@ public class JEIFoodMasterPlugin implements IModPlugin {
         registration.addRecipeCategories(new
                 PotBoilingRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
     }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.itemsList.get(0).get()),new RecipeType<>(PotBoilingRecipeCategory.UID, PotBlockRecipe.class));
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(PotBlockScreen.class, 110-23/2, 35, 23, 15, new RecipeType<>(PotBoilingRecipeCategory.UID, PotBlockRecipe.class));
+    }
+
+//    @Override
+//    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+//        registration.addRecipeTransferHandler(PotBlockMenu.class, new RecipeType<>(PotBoilingRecipeCategory.UID, PotBlockRecipe.class), 0, 12, 8, 36);
+//    }
+
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
