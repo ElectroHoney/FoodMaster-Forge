820789fbb674dc77178fb6be9a643c3d82460e4a
Mocanu Razvan <mocanurazvan92@yahoo.ro>
2022-07-15 22:52:26 +0300

    refactor: recipe type file added

---
+++ src/main/java/net/electrohoney/foodmastermod/recipe/ModRecipeTypes.java
@@ -0,0 +1,14 @@
+package net.electrohoney.foodmastermod.recipe;
+
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+
+import static net.minecraft.world.item.crafting.RecipeType.register;
+
+public interface ModRecipeTypes <T extends Recipe<?>>{
+
+    //RecipeType<CraftingRecipe> BAKING = register("baking");
+    //RecipeType<CraftingRecipe> BROILING = register("broiling");
+
+}
